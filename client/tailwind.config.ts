73799c3d9386276a2c1c919c9b0eb9562fcb4d54
EliteOath <elite.oath.ventures@gmail.com>
2025-12-24 16:53:11 -0800

    prelaunch

---
+++ client/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('tailwindcss-animate'),
+  ],
+}
+
+export default config
