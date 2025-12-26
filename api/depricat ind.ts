276bca8e45dd2a1c3f9ca729bba79b670fc444ab
EliteOath <elite.oath.ventures@gmail.com>
2025-12-26 12:48:46 -0800

    api index and trpc fix

--- api/depricat ind.ts
+++
@@ -1,6 +0,0 @@
-import type { IncomingMessage, ServerResponse } from "http";
-import app from "../server/_core/app";
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return app(req as any, res as any);
-}
