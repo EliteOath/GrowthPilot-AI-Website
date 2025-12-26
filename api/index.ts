261b38b67a5dfd99478885148e86b27a56e612e1
EliteOath <elite.oath.ventures@gmail.com>
2025-12-26 12:28:24 -0800

    api index fix

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import app from "../server/_core/app.js";
+import app from "../server/_core/app";
 
 export default function handler(req: IncomingMessage, res: ServerResponse) {
   return app(req as any, res as any);
