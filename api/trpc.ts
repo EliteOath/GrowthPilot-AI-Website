a64e28773a535f9e62eeae05b84683bc00e66941
EliteOath <elite.oath.ventures@gmail.com>
2025-12-26 20:33:00 -0800

    first compile

--- api/trpc.ts
+++
@@ -1,2 +0,0 @@
-import app from "../dist-server/_core/app.js";
-export default app;
---
+++ types/dist-backend.d.ts
@@ -0,0 +1,4 @@
+declare module "../dist-backend/api/server/_core/app.js" {
+  const app: any;
+  export default app;
+}
